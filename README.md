725421e1da17a89bc334ddffccd88847b758f2d9
Yehuda Cohen <78832113+YSCohen@users.noreply.github.com>
2026-03-10 00:11:54 -0400

    strftime, split App class use dt.strftime() instead of format(), chrono -> jiff split App.java into two classes, will hopefully add more generators

--- README.md
+++ README.md
@@ -7,12 +7,3 @@ To run, you need to have KosherJava installed in your local repository. In the K
 ```
 mvn clean install -Dmaven.test.skip=true -Dmaven.javadoc.skip=true
 ```
-
-Then, to generate the tests, run the following in this repo:
-```
-# for sea-level tests
-mvn exec:java -Dexec.mainClass="com.example.App" --quiet
-
-# for elevation-adjusted tests
-mvn exec:java -Dexec.mainClass="com.example.App" -Dexec.args="elev" --quiet
-```
--- src/main/java/io/github/YSCohen/rustZmanimTestGenerator/GenerateCzcTests.java
+++ src/main/java/io/github/YSCohen/rustZmanimTestGenerator/GenerateCzcTests.java
@@ -3,20 +3,19 @@ package io.github.YSCohen.rustZmanimTestGenerator;
 import java.lang.reflect.Method;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.TimeZone;
 
 import com.google.common.base.CaseFormat;
 import com.kosherjava.zmanim.ComprehensiveZmanimCalendar;
 import com.kosherjava.zmanim.util.GeoLocation;
 
-public class App {
+public class GenerateCzcTests {
     public static void main(String[] args) {
         boolean elev = (args.length > 0 && args[0].equals("elev"));
         generateAllZmanTests(elev);
     }
 
     private static void generateAllZmanTests(boolean useElevation) {
-        GeoLocation[] locs = getLocs();
+        GeoLocation[] locs = Helpers.getLocs();
 
         ComprehensiveZmanimCalendar czc = new ComprehensiveZmanimCalendar();
         Method[] methods = czc.getClass().getMethods();
@@ -53,7 +52,7 @@ public class App {
                 czcOfLocation.setUseElevation(useElevation);
 
                 Date value = (Date) method.invoke(czcOfLocation);
-                results[i] = iso(value, locs[i].getTimeZone());
+                results[i] = Helpers.formatDate(value, locs[i].getTimeZone());
             }
 
             String modifiedName = transformMethodName(method.getName());
@@ -79,7 +78,7 @@ public class App {
                                 for (czc, edt) in zip(cals, expected_datetime_strs) {
                                     let result = czc.%s().map_or_else(
                                         || String::from("None"),
-                                        |dt| dt.format("%s").to_string(),
+                                        |dt| dt.strftime("%s").to_string(),
                                     );
                                     assert_eq!(result, edt)
                                 }
@@ -94,46 +93,6 @@ public class App {
                     modifiedName, "%Y-%m-%d %H:%M:%S %Z");
         } catch (Exception e) {
             System.out.println("\n// Could not invoke " + method.getName());
-        }
-    }
-
-    private static GeoLocation[] getLocs() {
-        GeoLocation lakewood = new GeoLocation("LW", 40.0721087, -74.2400243, 15,
-                TimeZone.getTimeZone("America/New_York"));
-        GeoLocation samoa = new GeoLocation("SM", -13.8599098, -171.8031745, 1858,
-                TimeZone.getTimeZone("Pacific/Apia"));
-        GeoLocation jerusalem = new GeoLocation("JM", 31.7781161, 35.233804, 740.0,
-                TimeZone.getTimeZone("Asia/Jerusalem"));
-        GeoLocation los_angeles = new GeoLocation("LA", 34.0201613, -118.6919095, 71,
-                TimeZone.getTimeZone("America/Los_Angeles"));
-        GeoLocation tokyo = new GeoLocation("TK", 35.6733227, 139.6403486, 40,
-                TimeZone.getTimeZone("Asia/Tokyo"));
-        GeoLocation arctic_nunavut = new GeoLocation("AN", 81.7449398, -64.7945858, 127,
-                TimeZone.getTimeZone("America/Toronto"));
-        GeoLocation fiji = new GeoLocation("FJ", -17.633056, 178.016667, 1324,
-                TimeZone.getTimeZone("Pacific/Fiji"));
-        GeoLocation honolulu = new GeoLocation("HU", 21.466667, -157.966667, 10,
-                TimeZone.getTimeZone("America/Adak"));
-        GeoLocation niue = new GeoLocation("NI", -19.053006, -169.859199, 75,
-                TimeZone.getTimeZone("Pacific/Niue"));
-        GeoLocation[] locs = { lakewood, jerusalem, los_angeles, tokyo, arctic_nunavut, samoa, fiji, honolulu, niue };
-        return locs;
-    }
-
-    private static String iso(Date date, TimeZone timeZone) {
-        if (date == null) {
-            return "None";
-        } else {
-            Calendar cal = Calendar.getInstance(timeZone);
-            cal.setTime(date);
-            return String.format("%04d-%02d-%02d %02d:%02d:%02d %s",
-                    cal.get(Calendar.YEAR),
-                    cal.get(Calendar.MONTH) + 1,
-                    cal.get(Calendar.DAY_OF_MONTH),
-                    cal.get(Calendar.HOUR_OF_DAY),
-                    cal.get(Calendar.MINUTE),
-                    cal.get(Calendar.SECOND),
-                    timeZone.getDisplayName(cal.getTimeZone().inDaylightTime(date), TimeZone.SHORT));
         }
     }
 
